25cfd7883027dae150402848384c87b2f388d59e
Martin Seibert <martin.seibert@seibert.group>
2025-06-15 17:19:14 +0200

    refining vercel function

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,3 +1,7 @@
+export async function GET(request: Request) {
+    return new Response('Hello from Vercel!');
+}
+
 export async function POST(request: Request) {
     const body = await request.json();
     console.log(body);
