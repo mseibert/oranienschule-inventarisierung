3f8162c8e36bff31523aa632736fc6073714db2b
Martin Seibert <martin.seibert@seibert.group>
2025-06-15 17:16:35 +0200

    Turning into POST

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,3 +1,5 @@
-export function GET(request: Request) {
-    return new Response('Hello from Vercel!');
-  }
+export async function POST(request: Request) {
+    const body = await request.json();
+    console.log(body);
+    return new Response('Hello from Vercel!' + body);
+}
