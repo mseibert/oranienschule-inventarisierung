4a80b4c3ab4599aad0275898b11e59829e36a916
Martin Seibert <martin.seibert@seibert.group>
2025-06-15 13:43:45 +0200

    kicking astro serverless functions

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,26 +2,24 @@
 import { defineConfig } from 'astro/config';
 
 import vercel from '@astrojs/vercel';
-import vercelServerless from "@astrojs/vercel/serverless";
-import vercelStatic from "@astrojs/vercel/static";
 
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
   output: 'static',
-
   image: {
       domains: ["cloud.seatable.io"],
   },
-
-  adapter: vercelServerless({
+  adapter: vercel({
     webAnalytics: {
       enabled: true,
     },
   }),
-
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+  security: {
+    checkOrigin: false,
+  },
 });
