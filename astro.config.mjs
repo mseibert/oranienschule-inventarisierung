65dff9bd6af3c96f13e51d75467add9bab7b48d1
Martin Seibert <martin.seibert@seibert.group>
2025-06-16 16:15:31 +0200

    output SSR

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'static',
+  output: 'server',
   image: {
       domains: ["cloud.seatable.io"],
   },
