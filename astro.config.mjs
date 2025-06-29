3529d480c93e5f675cc45e9ff5c0681e8e8234e8
Martin Seibert <martin.seibert@seibert.group>
2025-06-29 19:10:38 +0200

    first attempt on Auth

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,18 +5,25 @@ import vercel from '@astrojs/vercel';
 
 import tailwindcss from '@tailwindcss/vite';
 
+import auth from 'auth-astro';
+
 // https://astro.build/config
 export default defineConfig({
   output: 'static',
+
   image: {
       domains: ["cloud.seatable.io"],
   },
+
   adapter: vercel({
     webAnalytics: {
       enabled: true,
     },
   }),
+
   vite: {
     plugins: [tailwindcss()]
   },
+
+  integrations: [auth()],
 });
